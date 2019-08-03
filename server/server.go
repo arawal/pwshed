f36e2fd7b647e10fe86288f9abad967df8ef1e69
Akshar <akshar.rawal@thomsonreuters.com>
2019-08-03 17:15:29 -0400

    graceful shutdown

--- server/server.go
+++ server/server.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/arawal/pwshed/hashlib"
@@ -11,7 +16,21 @@ import (
 // LaunchServer configures and launches a http server
 func LaunchServer() {
 	// Start and run the server
-	initRouter().Run(":8080")
+	router := initRouter()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	handleGracefulShutdown(srv)
 }
 
 // initRouter initializes a gin router with the preconfigured routes
@@ -48,6 +67,35 @@ func initRouter() *gin.Engine {
 			<-timer.C
 			c.String(http.StatusOK, result)
 		})
+
+		api.GET("/shutdown", func(c *gin.Context) {
+			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		})
 	}
 	return router
+}
+
+func handleGracefulShutdown(srv *http.Server) {
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Error shutting down server:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server gracefully exiting")
 }
